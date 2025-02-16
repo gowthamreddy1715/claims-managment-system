0b6f3632b46e10764c84b92e90cb48f0b8962e3d
gowthamreddy1715 <nallapareddygowtham@gmail.com>
2025-02-16 18:23:08 +0530

    good

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,33 +1,37 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const mongoose = require('mongoose')
-const {errorHandler} = require('./Middleware/errorMiddleware')
-const cors = require('cors')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const { errorHandler } = require("./Middleware/errorMiddleware");
+const cors = require("cors");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
-app.use(cors({origin:'*'}));
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(errorHandler)
+mongoose
+  .connect(
+    "mongodb+srv://nallapareddygowtham:Fortuner@gowtam.ubami.mongodb.net/cms?retryWrites=true&w=majority&appName=gowtam",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-mongoose.connect("mongodb+srv://shanmukhareddyvasa:shanmukha12345@shanmukhacluster.nmaie.mongodb.net/Claims-Management-System?retryWrites=true&w=majority&appName=ShanmukhaCLuster",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(()=>{
-    console.log("MongoDB Connected")
-}).catch(err=>{
-    console.error(err)
-})
-
-
-app.use('/users',require("./Routes/goUsers"))
-app.use('/policies',require("./Routes/goPolicy"))
-app.use('/claims',require("./Routes/goClaims"))
-app.use('/policyholder',require("./Routes/goPolicyholder"))
-app.listen(port,()=>{
-    console.log(`Server runs on port ${port}`)
-})
+app.use("/users", require("./Routes/goUsers"));
+app.use("/policies", require("./Routes/goPolicy"));
+app.use("/claims", require("./Routes/goClaims"));
+app.use("/policyholder", require("./Routes/goPolicyholder"));
+app.listen(port, () => {
+  console.log(`Server runs on port ${port}`);
+});
